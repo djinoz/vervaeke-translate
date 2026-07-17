6f7aef3ed812b3c20c7b5a9d4da62fff64976f42
djinoz <david@prismism.com>
2026-07-18 08:59:10 +1000

    chore: move deployed admin secret to firebase managed secret

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,19 @@
 import { onRequest } from 'firebase-functions/v2/https'
+import { defineSecret } from 'firebase-functions/params'
 
 import { createLocalServer, resolveLocalRuntime } from './server.js'
+
+const localSubmissionsAdminSecret = defineSecret('LOCAL_SUBMISSIONS_ADMIN_SECRET')
 
 let serverPromise
 
 async function getServer() {
   if (!serverPromise) {
     serverPromise = resolveLocalRuntime().then(({ repository, repositoryKind, firestoreBlocker, emailMode, emailBlocker, authService }) => {
+      const managedAdminSecret = localSubmissionsAdminSecret.value()
       return createLocalServer({
         repository,
+        adminSecret: managedAdminSecret || process.env.LOCAL_SUBMISSIONS_ADMIN_SECRET,
         repositoryKind,
         firestoreBlocker,
         emailMode,
@@ -25,6 +30,7 @@ export const api = onRequest({
   region: 'us-central1',
   cors: true,
   invoker: 'public',
+  secrets: [localSubmissionsAdminSecret],
 }, async (req, res) => {
   const server = await getServer()
   return server.emit('request', req, res)
